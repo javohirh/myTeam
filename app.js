21eb7826535789ed0fd31487e61393cc122d3017
javohirh <khasanovjavokhir2004@gmail.com>
2024-06-08 15:57:57 +0500

    first section

---
+++ app.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("hamburber-menu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
